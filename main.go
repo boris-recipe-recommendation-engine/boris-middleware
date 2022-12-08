de6d3b7c2d30505c9900ccbf217e59dd6d560301
0xDEADBEAT <jasperbriggs677@gmail.com>
2022-12-08 14:04:22 -0500

    populate

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"log"
+
+	paths "boris-middleware/paths"
+
+	"github.com/gofiber/fiber/v2"
+
+	"database/sql"
+
+	"github.com/go-sql-driver/mysql"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// Database instance
+var db *sql.DB
+
+// Connect function
+func Connect() error {
+	cfg := mysql.Config{
+        User:   "root",
+        Passwd: "0o0p0o0p",
+        Net:    "tcp",
+        Addr:   "127.0.0.1:3306",
+        DBName: "boris_recipes",
+    }
+
+	var err error
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+	if err != nil {
+		return err
+	}
+	if err = db.Ping(); err != nil {
+		return err
+	}
+	return nil
+}
+
+
+func main(){
+
+    if err := Connect(); err != nil {
+		log.Fatal(err)
+	}
+
+	method_resolver := paths.CookingMethodTable(db)
+
+    app := fiber.New()
+
+    app.Post("/recipes", method_resolver)
+
+    log.Fatal(app.Listen(":3000"))
+}
---
+++ schemas/table.go
@@ -0,0 +1,22 @@
+package schemas
+
+type CookingMethodTable struct {
+	Beef        bool `json:"beef,omitempty"`
+	BlackPepper bool `json:"black_pepper,omitempty"`
+	Butter      bool `json:"butter,omitempty"`
+	Chicken     bool `json:"chicken,omitempty"`
+	Eggs        bool `json:"eggs,omitempty"`
+	Flour       bool `json:"flour,omitempty"`
+	Milk        bool `json:"milk,omitempty"`
+	Oil         bool `json:"oil,omitempty"`
+	Paprika     bool `json:"paprika,omitempty"`
+	Parsley     bool `json:"parsley,omitempty"`
+	Pork        bool `json:"pork,omitempty"`
+	Rice        bool `json:"rice,omitempty"`
+	Salt        bool `json:"salt,omitempty"`
+	Star_anise  bool `json:"star_anise,omitempty"`
+	Sugar       bool `json:"sugar,omitempty"`
+	Tofu        bool `json:"tofu,omitempty"`
+	Vanilla     bool `json:"vanilla,omitempty"`
+	Water       bool `json:"water,omitempty"`
+}
